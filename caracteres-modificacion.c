6c6b12fc973560faeaf91f67f441b48bf0bec076
victor-huallpa <vech.600@gmail.com>
2025-08-29 11:10:54 -0500

    formas de imprimir o tomar caracteres

---
+++ caracteres-modificacion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    %c: unico caracter
+    %d: entero
+    %u: entero sin signo en base decimal
+    %o: entero en base octal
+    %x: entero en base hexadecimal
+    %e: un numero real en coma flotante, con exponente
+    %f: un numero real en coma flotante, sin exponente
+    %s: una cadena de catacteres.
+    %p: puntero o direccion de memoria
+    */
+
+    int numero = 23;
+    float numf = 12.34;
+    
+
+    return 0;
+}
