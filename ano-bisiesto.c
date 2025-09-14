ca1ddfa06a68f53575e3816f4b9f5a46ad02f342
victor-huallpa <vech.600@gmail.com>
2025-09-13 19:53:14 -0500

    anio bisiesto con funciones

--- ano-bisiesto.c
+++ ano-bisiesto.c
@@ -1,33 +1,43 @@
-/*
-Crea un algoritmo que nos ayude a determinar si el ano ingresado por el 
-usuario es bisiesto o no.
-CONSIDERACIONES PARA ANO BISIESTO:
-    - Numero de 4 cifras
-    - Son multiplos de 4 pero no multiplos de 100
-    - Son muliplos de 400
-*/
-
 #include <stdio.h>
 
-int main(){
-    //variables
-    int anio;
+int getDate();
+const char* anioBisiesto(int anio);
 
-    //solicitamos que ingrese el ano al usuario
+int main() {
+    int anio = 1;
+    const char *message;
+
+    printf("Bienvenido a nuestra app para calcular si un anio es bisiesto.");
+    while (anio != 0) {
+        printf("\nIngrese '0' para salir.\n");
+        anio = getDate();
+        if (anio != 0 && anio > 999) {
+            message = anioBisiesto(anio);
+            printf("%s\n", message);
+            continue;
+        } else if (anio <= 999 && anio != 0) {
+            printf("\nIngrese un anio valido\nVuelva a intentarlo.\n");
+            continue;
+        }
+    }
+
+    printf("\nGracias por usar nuestra app\n");
+    return 0;
+}
+
+int getDate() {
+    int anio;
     printf("Ingrese el anio a evaluar: ");
     scanf("%d", &anio);
+    return anio;
+}
 
-    //calculamos si el anio es bisiesto o no
-    if(anio %400 == 0){
-        printf("El anio es Bisiesto.");
+const char* anioBisiesto(int anio) {
+    if (anio % 400 == 0) {
+        return "El anio es bisiesto.";
+    } else if ((anio % 4 == 0) && (anio % 100 != 0)) {
+        return "El anio es bisiesto.";
+    } else {
+        return "El anio es comun.";
     }
-    else if ((anio % 4 == 0) && (anio % 100 != 0))
-    {
-        printf("El anio es bisiesto.");
-    }else{
-        printf("El anio es comun.");
-    }
-    
-
-    return 0;
 }
