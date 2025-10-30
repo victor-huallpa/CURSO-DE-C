7da95073fe0e284cdaebc97a6915c93490103b0b
victor-huallpa <vech.600@gmail.com>
2025-10-30 12:41:57 -0500

    funciones

---
+++ c_plus_plus/cumpleanios.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void felizcumpleanos();
+
+int main (){
+
+    felizcumpleanos();
+
+    return 0;
+
+}
+
+void felizcumpleanos(){
+    cout<<"que los cumplas feliz\n";
+    cout<<"que los cumplas feliz\n";
+    cout<<"que los cumplas a ti]\n";
+    cout<<"que los cumplas feliz\n";
+}
+
