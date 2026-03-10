aca03e2b67f3b3dc82c57ce8a0d37bf6a541c313
Victor Huallpa <vech.600@gmail.com>
2026-03-10 13:51:47 -0500

    header clietne

---
+++ ejercicicios/banca/Cliente.h
@@ -0,0 +1,22 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include "Persona.h"
+#include <string>
+
+using namespace std;
+
+class Cliente : public Persona
+{
+private:
+    int codigoCliente;
+    
+public:
+    Cliente(string nombre, int edad, string dni, int codigoCliente);
+    void mostrarCliente();
+};
+
+
+
+
+#endif
