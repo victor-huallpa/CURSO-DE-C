ff55602eaf336c44f91c7da45eecbe32c54b86bf
Victor Huallpa <vech.600@gmail.com>
2026-03-14 22:31:05 -0500

    clase persona

---
+++ ejercicicios/persona/Persona.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Persona.h"
+
+using namespace std;
+
+Persona::Persona(float e, float p, string d, string n){
+    this->estatura = e;
+    this->peso = p;
+    this->dni = d;
+    this->nombre = n;
+}
+
+void Persona::trabajar(){
+    cout<<nombre<<" esta trabajndo\n";
+}
+
+void Persona::comer(){
+    cout<<nombre<<" esta comiendol.\n";
+}
+void Persona::getDatos(){
+    cout<<nombre<<endl;
+    cout<<estatura<<endl;
+    cout<<peso<<endl;
+    cout<<dni<<endl;
+}
