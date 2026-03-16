dcf49a1b933560d67c20db757ac7da85a4da90ea
Victor Huallpa <vech.600@gmail.com>
2026-03-15 21:18:16 -0500

    funcion principal.

--- ejercicicios/bancaSimple/main.cpp
+++ ejercicicios/bancaSimple/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+// #include <tream>
+#include <string>
 #include "Banco.h"
 
 using namespace std;
 
+// class Start
+// {
+// private:
+//     int numero;
+//     string cadena;
+// public:
+//     Start(int numero, string c){
+//         this->numero = numero;
+//         this->cadena = c;
+//         cout<<numero<<endl;
+//         if (numero == 0)
+//         {
+//            cout<<"hola";
+//         }
+        
+//     }
+
+//     void setDatos(){
+//         cout<<numero<<"\n"<<cadena;
+//     }
+// };
+
+using namespace std;
+
 int main(){
-    Banco c1;
-    c1.mostrarCuentas();
+    Banco p1;
+    p1.mostrarCuentas();
+    // 
+    // Start p1(12, "hola");
+    // p1.setDatos();
+
+
+    // Banco c1;
+    // c1.mostrarCuentas();
 }
