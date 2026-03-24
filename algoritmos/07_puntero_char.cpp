af6b40e760c4fc46c246fb5262080182881b2ac5
Victor Huallpa <vech.600@gmail.com>
2026-03-24 15:25:06 -0500

    Contando elementos tipo string con putneros

---
+++ algoritmos/07_puntero_char.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int longitud(char *str);
+
+using namespace std;
+
+int main(){
+    string str = "pedrito";
+
+
+    cout<<"La lognitud del string '"<<str<<"' es de: "<<longitud(&str[0])<<endl;
+
+
+    return 0;
+}
+
+int longitud(char *str){
+    int cont = 0;
+    while (*(str+cont) != '\0')
+    {
+        cont += 1;
+        // str++;
+        // cout<<"hello"<<endl;
+    }
+    return cont;
+
+}
