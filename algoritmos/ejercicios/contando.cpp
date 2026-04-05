b94e7d6e388c729db82db5b2d1a494865ba9ebf0
victor-huallpa <vech.600@gmail.com>
2026-04-04 22:40:15 -0500

    fidex: solucionando problemas de compilaicon

---
+++ algoritmos/ejercicios/contando.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+
+int main(){
+
+    for (int  i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cout<<i<<j<<std::endl;
+        }
+        
+    }
+    
+    return 0;
+}
