676daeaa1ad2f449cd3d12b0b4b9c71d3512de20
victor-huallpa <vech.600@gmail.com>
2026-05-09 21:04:15 -0500

    feat: ejercicio de conjuntos 7

---
+++ estructuras_discreteas/conjuntos/07_ejercicio.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int lleno = 40;
+    int vacio = 50;
+    int intermedio = 10;
+
+    // Total de usuarios
+    int total = lleno + vacio + intermedio;
+
+    cout << "Total de usuarios del sistema: ";
+    cout << total << endl;
+
+    return 0;
+}
