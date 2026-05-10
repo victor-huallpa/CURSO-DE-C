89f111d51f963adae6d68ff4e2500f2f16283755
victor-huallpa <vech.600@gmail.com>
2026-05-09 21:02:59 -0500

    feat: Ejercicio de conjunto 4

---
+++ estructuras_discreteas/conjuntos/04_ejercicio.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int publican = 150;
+    int leen = 200;
+    int ambos = 50;
+
+    // Total de usuarios
+    int total = publican + leen - ambos;
+
+    cout << "Total de usuarios en la red social: ";
+    cout << total << endl;
+
+    return 0;
+}
