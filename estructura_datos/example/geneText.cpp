f09d891689e4e9b1336e0d1cd5e85893e1752573
victor-huallpa <vech.600@gmail.com>
2026-05-11 17:39:36 -0500

    feat: generador de texto en txt

---
+++ estructura_datos/example/geneText.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+
+int main(){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, 25);
+
+    int numPa;
+    int numCarac = 5;
+
+    std::cout<<"Cuantas palabras qeuire generar: ";
+    std::cin>>numPa;
+
+    std::cout<<"Cuantos caracteres cada palabra:";
+    std::cin>>numCarac;
+
+    std::ofstream archi("lineas_text.txt");
+
+
+    std::string linea = "";
+
+    for(int i = 0; i < numPa; i++){
+        linea = "";
+
+        linea = "Linea " + std::to_string(i+1);
+
+
+        // for (int j = 0; j < numCarac; j++)
+        // {
+        //     int n = dist(gen);
+        //     char let = 'a' + n;
+        //     linea += let;
+        // }
+        if (i == numPa-1)
+        {
+            archi << linea;
+            continue;
+        }
+        
+        archi << linea << std::endl;
+    }
+    // std::ofstream archi("lineas_text.txt");
+
+    // archi << linea;
+    archi.close();
+
+    return 0;
+}
