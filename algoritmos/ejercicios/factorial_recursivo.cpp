dee2f6e823a8acf064af5fd13ef48cb372845d63
victor-huallpa <vech.600@gmail.com>
2026-05-14 18:29:03 -0500

    feat: factoiral de un numero

--- algoritmos/ejercicios/factorial_recursivo.cpp
+++ algoritmos/ejercicios/factorial_recursivo.cpp
@@ -5,12 +5,18 @@ using namespace std;
 int factorial(int num);
 
 int main () {
-    cout<<factorial(5)<<endl;
+    int fac;
+    cout<<"Calculando factorial...\n";
+    cout<<"Ingrese numero: ";
+    cin>>fac;
+
+    cout<<"El dactoria es: "<<factorial(fac)<<endl;
 
     return 0;
 }
 
 int factorial(int num){
+    // cout<<num<<endl;
     if(num == 0){
         return 1;
     }else{
